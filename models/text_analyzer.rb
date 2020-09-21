cffc88ad4af66240531b0a3f7fc69c4246387eb3
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-21 13:18:53 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -41,11 +41,5 @@ class TextAnalyzer
     biggest
   end
   
-  post '/' do
-    text_from_user = params[:user_text]
-   
-    @analyzed_text = TextAnalyzer.new(text_from_user)
-   
-    erb :results
-  end
+  
 end
